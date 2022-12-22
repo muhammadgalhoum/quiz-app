94f1001680e71a49cbd7ba62b9e242f11436ad28
muhammadgalhoum <56798376+muhammadgalhoum@users.noreply.github.com>
2022-12-22 21:17:41 +0200

    Update home.js

--- js/home.js
+++ js/home.js
@@ -1,11 +1,11 @@
 let testEn = document.getElementById("testEn");
 testEn.onclick = function () {
-  window.location.href = "quiz-app/index-en.html";
+  window.location.href = "index-en.html";
   testEn.checked = false;
 }
 
 let testAr = document.getElementById("testAr");
 testAr.onclick = function () {
-  window.location.href = "quiz-app/index-ar.html";
+  window.location.href = "index-ar.html";
   testAr.checked = false;
 };
